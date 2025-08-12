87876354a932efacdca8bbaf1a79211ec5442e74
lucas <lucas.hamacher@ma-versand.de>
2025-08-12 13:07:03 +0200

    add start screen and set up game field

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+
+}
---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-start-screen',
+  imports: [RouterModule],
+  templateUrl: './start-screen.component.html',
+  styleUrl: './start-screen.component.scss'
+})
+export class StartScreenComponent {
+
+  constructor(private router: Router) { }
+  newGame() {
+    this.router.navigateByUrl('/game')
+  }
+}
