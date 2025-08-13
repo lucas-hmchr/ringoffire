1aaafa0fe34457089579de08c014bfad2989c966
lucas <lucas.hamacher@ma-versand.de>
2025-08-13 13:16:38 +0200

    add card selection with animation and player display

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,17 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
 
 @Component({
   selector: 'app-game',
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 
   pickCardAnimation = false;
-  
+  currentCard: string = '';
+  game: Game | undefined;
+
+  ngOnInit() {
+    this.newGame()
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game?.stack.pop() ?? '';
+      this.pickCardAnimation = true;
+      console.log(this.game, this.currentCard)
+
+      setTimeout(() => {
+        this.game?.playedCards.push(this.currentCard)
+        this.pickCardAnimation = false;
+      }, 1000);
+
+    }
   }
 }
