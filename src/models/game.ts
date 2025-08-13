16638ccb2a9513299169667b7e83ea3695746532
lucas <lucas.hamacher@ma-versand.de>
2025-08-13 13:15:59 +0200

    add game class

---
+++ src/models/game.ts
@@ -0,0 +1,27 @@
+export class Game {
+    public players: string[] = ['Hans', 'Peter', 'Rüdiger'];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('hearts_' + i);
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+        }
+        this.shuffle(this.stack)
+        console.log(this.stack)
+    }
+
+    shuffle(array: string[]) {
+        let currentIndex = array.length;
+        while (currentIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+    }
+}
