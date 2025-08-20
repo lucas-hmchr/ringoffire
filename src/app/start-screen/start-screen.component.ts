0c5fcd8fdf074ced1e4054235e9c1576d010cc06
lucas <lucashamacher01@gmail.com>
2025-08-20 13:04:32 +0200

    create a new game

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { addDoc, collection, Firestore } from '@angular/fire/firestore';
 import { Router, RouterModule } from '@angular/router';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-start-screen',
@@ -8,9 +10,19 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+  private firestore: Firestore = inject(Firestore);
 
   constructor(private router: Router) { }
-  newGame() {
-    this.router.navigateByUrl('/game')
+
+  async newGame() {
+    let game = new Game();
+    await addDoc(this.gamesRef(), game.toJson()).then((gameInfo: any) => {
+      console.log(gameInfo)
+    this.router.navigateByUrl('/game/' + gameInfo.id)
+    })
+  }
+
+  gamesRef() {
+    return collection(this.firestore, 'games');
   }
 }
